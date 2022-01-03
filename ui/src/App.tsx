90f719ba3e460c0b4030310912daa1a4a793b24c
Hung-Yi Loo <hungyi.loo@gmail.com>
2022-01-03 17:06:52 +0800

    Working on UX

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -19,7 +19,6 @@ function Chat() {
   const myUserId = useMemo(() => nanoid(), []);
 
   const [chat, setChat] = useState<Message[]>([]);
-  const [draft, setDraft] = useState("");
 
   const base = process.env.REACT_APP_WS_URL === '/'
     ? document.location.origin.replace("https:", "wss:").replace("http:", "ws:")
@@ -50,81 +49,136 @@ function Chat() {
   }, [lastMessage])
 
   useEffect(() => {
-    setChat([])
+    setChat([{
+      id: nanoid(),
+      user: nanoid(),
+      content:
+`Hi there! Need to send secure and temporary messages to someone? Heisenpad can help.
+
+Use heisenpad to send short-lived messages to people only in this channel. Messages only exist between senders & receivers. No logging. No persisting. Refresh and they're gone!
+
+You're currently in the [${channel}] channel. You can change channels at the top of your screen.
+
+Activate end-to-end encryption by *setting a key* at the top right of the screen. Any listeners will need the same key to read your encrypted messages.`
+    }])
   }, [channel])
 
-  function send(cmd: Command) {
-    sendMessage(JSON.stringify(cmd));
-    setDraft("");
-  }
+  function send(cmd: Command) { sendMessage(JSON.stringify(cmd)); }
 
-  function put() {
+  function put(draft: string) {
     send({
       command: "put",
       message: { id: nanoid(), user: myUserId, content: draft.trim() }
     });
   }
 
-  const multiline = draft.includes("\n")
-
   return <div className="flex justify-center">
     <div className="container flex flex-col h-screen px-2">
       <div className="flex items-center pt-6 pb-4 px-2 border-b-4 border-zinc-800">
+        <svg
+          className={`h-7 mr-2 -mt-2 -mb-1`}
+          viewBox="0 0 100 100">
+          <use xlinkHref="/heisenpad.svg#icon-logo" />
+        </svg>
         <div className="text-lg tracking-wider font-display text-zinc-100">
-          HEISEN<span className="text-amber-400">PAD</span>
+          HEISENPAD
         </div>
-        <div className="text-base tracking-wider font-display text-cyan-500 ml-2">
-          <span className="text-zinc-700">#</span>
+        <span className="font-display text-zinc-500 ml-4 text-xl">@</span>
+        <div className="tracking-wider font-display text-amber-500 hover:text-amber-300 cursor-pointer">
           {channel}
+        </div>
+        <div className="ml-auto">
+          <Key active/>
         </div>
       </div>
       <div className="flex-grow overflow-auto px-4 py-2">
         {chat.map(msg => <React.Fragment key={msg.id}>
           <Bubble
             position={msg.user === myUserId ? 'right' : 'left'}
+            onResend={() => send({ command: "put", message: msg })}
             onDelete={() => send({ command: "delete", id: msg.id })}>
             {msg.content}
           </Bubble>
         </React.Fragment>)}
       </div>
-      <form className="flex-shrink-0 flex items-end pt-4 pb-8 px-4 border-zinc-800" onSubmit={e => { e.preventDefault(); put() }}>
-        <div className="flex-grow flex-col items-stretch">
-          {multiline && <div className="text-sm font-mono text-cyan-500 mb-3">
-            Multi&ndash;line mode activated. The <Key>Enter</Key> key will now insert a new line. Press <Key>CTRL+⏎</Key> or click <Key>SEND</Key> when you&rsquo;re done.
-          </div>}
-          <Input
-            value={draft}
-            onChange={e => setDraft(e.target.value)}
-            onKeyPress={e => {
-              if (e.code === "Enter" && ((!multiline && !e.shiftKey) || e.ctrlKey)) {
-                e.preventDefault();
-                put();
-              }
-            }}
-            rows={multiline ? 5 : 1}
-            placeholder="Type something..."
-            className="w-full" />
-        </div>
-        <Button
-          type="submit"
-          className="ml-8"
-          style={{ marginBottom: 6 }}
-          disabled={!draft.trim()}
-          color="amber">
-          SEND
-        </Button>
-      </form>
+      <Editor onDone={put}/>
     </div>
   </div>
 }
 
-function Key(props: { children: React.ReactNode }) {
+function Editor(props: { onDone: (value: string) => void }) {
+  const [draft, setDraft] = useState("");
+  const multiline = draft.includes("\n")
+
+  function handleDone() {
+    props.onDone(draft)
+    setDraft("")
+  }
+
+  return <form className="flex-shrink-0 flex items-end pt-4 pb-8 px-4 border-zinc-800" onSubmit={e => { e.preventDefault(); handleDone() }}>
+    <div className="flex-grow flex-col items-stretch">
+      {multiline
+        ? <div className="text-sm font-mono text-cyan-500 mb-3">
+          Multi&ndash;line mode activated. Press <KeyboardShortcut>CTRL+⏎</KeyboardShortcut> or click <KeyboardShortcut>SEND</KeyboardShortcut> when you&rsquo;re done.
+        </div>
+        : <div className="text-sm font-mono text-zinc-500 mb-3">
+          Press <span className="border border-zinc-700 px-1 bg-zinc-800 text-zinc-400">Shift+⏎</span> to activate multi&ndash;line mode
+        </div>}
+      <Input
+        value={draft}
+        onChange={e => setDraft(e.target.value)}
+        onKeyDown={e => {
+          // A hack to allow basic tab insertion in text editor
+          if (e.code === 'Tab') {
+            e.preventDefault();
+            const me = (e.target as HTMLTextAreaElement);
+            const start = me.selectionStart;
+            const end = me.selectionEnd;
+            me.value = me.value.substring(0, start) + "\t" + me.value.substring(end);
+            me.selectionStart = me.selectionEnd = start + 1;
+          }
+        }}
+        onKeyPress={e => {
+          if (e.code === "Enter" && ((!multiline && !e.shiftKey) || e.ctrlKey)) {
+            e.preventDefault();
+            handleDone()
+          }
+        }}
+        rows={multiline ? 5 : 1}
+        placeholder="Type something..."
+        className="w-full" />
+    </div>
+    <Button
+      type="submit"
+      className="ml-8"
+      style={{ marginBottom: 6, height: "3.75rem" }}
+      disabled={!draft.trim()}
+      color="amber">
+      SEND
+    </Button>
+  </form>
+}
+
+function KeyboardShortcut(props: { children: React.ReactNode }) {
   return <span className="border border-cyan-600 px-1 bg-cyan-900 text-cyan-50">{props.children}</span>
 }
 
+function Key(props: { active?: boolean }) {
+  return <div className="flex items-center">
+    <svg
+      className={`h-5 retro-box mr-4 ${props.active ? 'text-zinc-900 bg-emerald-500 emerald' : 'text-rose-400 bg-rose-900 rose-900 animate-pulse'}`}
+      viewBox="0 0 100 100">
+      <use xlinkHref="/key.svg#icon-key" />
+    </svg>
+    {props.active
+      ? <span className="font-display text-sm text-emerald-500 mt-1">Secure</span>
+      : <span className="font-display text-sm text-rose-700 mt-1">Unsecure</span>}
+  </div>
+}
+
 type Command =
-  { command: "put", message: Message } |
-  { command: "delete", id: string }
+  | { command: "put", message: Message }
+  | { command: "delete", id: string }
 
 interface Message {
   id: string
--- ui/src/Bubble.tsx
+++ ui/src/Bubble.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./Bubble.css";
 
-export default function Bubble(props: { position: 'left' | 'right', children?: React.ReactNode, onDelete?: () => void }) {
+export default function Bubble(props: { position: 'left' | 'right', children?: React.ReactNode, onDelete?: () => void, onResend?: () => void }) {
   const [highlighted, setHighlighted] = useState(false)
 
   function copyToClipboard() {
     const newClip = props.children?.toString()
     if (!newClip) return
     navigator.clipboard.writeText(newClip)
+  }
+
+  function highlight() {
     setHighlighted(true)
     setTimeout(() => setHighlighted(false), 300);
   }
@@ -19,13 +22,18 @@ export default function Bubble(props: { position: 'left' | 'right', children?: R
     <div className="chat-tools">
       <button
         onClick={() => props.onDelete?.()}
-        className="text-rose-500 hover:text-rose-600 hover:underline">
+        className="text-rose-500 hover:text-rose-300">
         DELETE
       </button>
       <button
-        onClick={() => copyToClipboard()}
-        className="text-cyan-500 hover:text-cyan-600 hover:underline">
+        onClick={() => { copyToClipboard(); highlight(); }}
+        className="text-cyan-500 hover:text-cyan-300">
         COPY
+      </button>
+      <button
+        onClick={() => { props.onResend?.(); highlight() }}
+        className="text-emerald-500 hover:text-emerald-300">
+        RESEND
       </button>
     </div>
     <div className="chat-bubble-arrow"/>
